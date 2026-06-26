ba68486bf9bc17daf5e929c1b2662749ca80a7bf
Your Name <you@example.com>
2026-06-26 18:31:56 +0700

    fix bug

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -17,19 +17,27 @@ export function Footer() {
         <div>
           <h2>Lịch vạn niên</h2>
           <Link href="/lich-hom-nay">Lịch hôm nay</Link>
+          <Link href="/am-lich-hom-nay">Âm lịch hôm nay</Link>
           <Link href="/lich-van-nien">Lịch vạn niên</Link>
           <Link href={`/am-lich/nam/${currentYear}`}>Lịch âm {currentYear}</Link>
         </div>
         <div>
-          <h2>Tiện ích</h2>
+          <h2>Ngày tốt · Giờ tốt</h2>
+          <Link href="/ngay-tot-xau-hom-nay">Ngày tốt xấu hôm nay</Link>
+          <Link href="/gio-hoang-dao-hom-nay">Giờ hoàng đạo hôm nay</Link>
+          <Link href="/ngay-tot-xau">Xem ngày tốt xấu theo ngày</Link>
+          <Link href="/gio-hoang-dao">Xem giờ hoàng đạo theo ngày</Link>
+          <Link href="/con-bao-nhieu-ngay-den-tet">Còn bao nhiêu ngày đến Tết</Link>
+          <Link href={`/tet/${currentYear + 1}`}>Tết {currentYear + 1} ngày nào?</Link>
+        </div>
+        <div>
+          <h2>Công cụ</h2>
           <Link href="/chuyen-doi-lich">Đổi ngày âm dương</Link>
-          <Link href="/tinh-tuoi-am">Đổi tuổi / tính tuổi âm</Link>
-          <Link href="/gio-hoang-dao">Giờ hoàng đạo</Link>
-          <Link href="/ngay-tot-xau">Ngày tốt xấu</Link>
+          <Link href="/sinh-nam/1990">Sinh năm bao nhiêu tuổi</Link>
+          <Link href="/tinh-tuoi-am">Tính tuổi âm</Link>
           <Link href="/dem-ngay">Đếm ngày</Link>
           <Link href="/lich-nghi-le">Lịch nghỉ lễ</Link>
           <Link href="/nhac-ngay-gio">Nhắc ngày giờ</Link>
-          <Link href="/am-lich-hom-nay">Âm lịch hôm nay</Link>
         </div>
         <div>
           <h2>Liên hệ</h2>
